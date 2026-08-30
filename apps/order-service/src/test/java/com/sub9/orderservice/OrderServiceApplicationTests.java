a1a6e508b38e01488ae6d62fa77abc38934aeccd
ByeongGyu An <an622911@gmail.com>
2026-08-31 01:38:13 +0900

    test(app): Context 테스트 외부 의존성 격리

--- apps/order-service/src/test/java/com/sub9/orderservice/OrderServiceApplicationTests.java
+++ apps/order-service/src/test/java/com/sub9/orderservice/OrderServiceApplicationTests.java
@@ -3,7 +3,13 @@ package com.sub9.orderservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+        "spring.cloud.config.enabled=false",
+        "eureka.client.enabled=false",
+        "spring.datasource.url=jdbc:h2:mem:order-service;MODE=PostgreSQL;DB_CLOSE_DELAY=-1",
+        "spring.jpa.hibernate.ddl-auto=none",
+        "management.tracing.export.enabled=false"
+})
 class OrderServiceApplicationTests {
 
     @Test
