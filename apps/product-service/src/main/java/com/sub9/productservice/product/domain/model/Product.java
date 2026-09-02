6212d9e209f042df9e31ef1122fad5e48230f757
cpfur18 <cpfur18@gmail.com>
2026-09-03 07:36:21 +0900

    feat(product): 누락된 Product Entity Update 로직 추가

--- apps/product-service/src/main/java/com/sub9/productservice/product/domain/model/Product.java
+++ apps/product-service/src/main/java/com/sub9/productservice/product/domain/model/Product.java
@@ -1,6 +1,5 @@
 package com.sub9.productservice.product.domain.model;
 
-import com.github.f4b6a3.uuid.UuidCreator;
 import com.sub9.common.exception.BusinessException;
 import com.sub9.productservice.common.entity.BaseEntity;
 import com.sub9.productservice.product.domain.exception.ProductErrorCode;
@@ -63,6 +62,7 @@ public class Product extends BaseEntity {
   }
 
   public void update(String name, String content) {
-
+    this.name = name;
+    this.content = content;
   }
 }
