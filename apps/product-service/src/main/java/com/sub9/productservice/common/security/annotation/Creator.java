05dacdfda3930e662428e3ce7f36062fe88bfa0e
cpfur18 <cpfur18@gmail.com>
2026-09-03 08:48:04 +0900

    feat(product): 권한 커스텀 어노테이션 추가

---
+++ apps/product-service/src/main/java/com/sub9/productservice/common/security/annotation/Creator.java
@@ -0,0 +1,13 @@
+package com.sub9.productservice.common.security.annotation;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@PreAuthorize("hasRole('CREATOR')")
+public @interface Creator {}
