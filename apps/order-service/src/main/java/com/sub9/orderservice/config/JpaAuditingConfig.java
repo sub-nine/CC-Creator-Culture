21effdcc9bf34159f18b3a4cc6b83de646a75536
ByeongGyu An <an622911@gmail.com>
2026-09-03 19:11:40 +0900

    feat: 주문 애그리거트와 엔티티 매핑 구현

---
+++ apps/order-service/src/main/java/com/sub9/orderservice/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package com.sub9.orderservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+}
---
+++ apps/order-service/src/main/java/com/sub9/orderservice/order/domain/model/OrderItemStatus.java
@@ -0,0 +1,10 @@
+package com.sub9.orderservice.order.domain.model;
+
+public enum OrderItemStatus {
+    ORDERED,
+    PREPARING,
+    SHIPPED,
+    DELIVERED,
+    COMPLETED,
+    CANCELED
+}
