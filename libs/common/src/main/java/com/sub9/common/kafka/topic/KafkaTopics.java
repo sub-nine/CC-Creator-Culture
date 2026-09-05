25a478426a00d107c9e14d2abe8bdef916f62d9c
cpfur18 <cpfur18@gmail.com>
2026-09-05 13:43:10 +0900

    feat(product): 카프카 이벤트 Common 모듈에 추가 - rename: 카프카 토픽 명 변경

--- libs/common/src/main/java/com/sub9/common/kafka/topic/KafkaTopics.java
+++ libs/common/src/main/java/com/sub9/common/kafka/topic/KafkaTopics.java
@@ -6,6 +6,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class KafkaTopics {
   public static final String PRODUCT_CREATED = "product.created";
-  public static final String PRODUCT_VIEWED = "product_viewed";
+  public static final String PRODUCT_VIEW_COUNT_SYNC = "product.view-count.sync";
   public static final String ORDER_PAID = "order.paid";
 }
