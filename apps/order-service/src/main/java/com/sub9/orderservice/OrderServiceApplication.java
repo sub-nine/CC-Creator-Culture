18176ec474e41dda8275ba3172d5e3fedd4df01b
cpfur18 <cpfur18@gmail.com>
2026-09-06 07:35:59 +0900

    chore(order): OpenFeign 타임아웃 및 에러 로그 설정

--- apps/order-service/src/main/java/com/sub9/orderservice/OrderServiceApplication.java
+++ apps/order-service/src/main/java/com/sub9/orderservice/OrderServiceApplication.java
@@ -2,7 +2,9 @@ package com.sub9.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class OrderServiceApplication {
 
