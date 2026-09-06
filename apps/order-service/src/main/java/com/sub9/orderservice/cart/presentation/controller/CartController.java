58b802e825512888db05c1f4db9dca0ab0a2c8b4
cpfur18 <cpfur18@gmail.com>
2026-09-06 13:34:56 +0900

    feat(order) 장바구니 등록 API 구현

---
+++ apps/order-service/src/main/java/com/sub9/orderservice/cart/presentation/controller/CartController.java
@@ -0,0 +1,25 @@
+package com.sub9.orderservice.cart.presentation.controller;
+
+import com.sub9.common.dto.response.ApiResponse;
+import com.sub9.orderservice.cart.application.service.CartService;
+import com.sub9.orderservice.cart.presentation.request.AddCartItemRequest;
+import com.sub9.orderservice.common.security.GatewayAuthenticationPrincipal;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/v1/cart/items")
+public class CartController {
+  private final CartService cartService;
+
+  @PostMapping
+  public ApiResponse<Void> addCartItem(
+      @AuthenticationPrincipal GatewayAuthenticationPrincipal principal,
+      @Valid @RequestBody AddCartItemRequest request) {
+    cartService.addCartItem(request.toCommand(principal.userId()));
+    return ApiResponse.success("장바구니 등록 성공", null);
+  }
+}
