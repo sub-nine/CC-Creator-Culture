85d4009ab3972c8ebb2fd8de8a9f5ba93f37c9d7
cpfur18 <cpfur18@gmail.com>
2026-09-06 13:37:07 +0900

    feat(order): CartProduct Feign 클라이언트 구현

---
+++ apps/order-service/src/main/java/com/sub9/orderservice/cart/infrastructure/feign/CartProductFeignClient.java
@@ -0,0 +1,24 @@
+package com.sub9.orderservice.cart.infrastructure.feign;
+
+import com.sub9.orderservice.cart.application.dto.CartItemProducInfo;
+import com.sub9.orderservice.config.OpenFeignConfig;
+import java.util.List;
+import java.util.UUID;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+// TODO : URL 경로가 변경되어 Merge 이후 Product에서 수정 필요
+@FeignClient(
+    name = "product-service",
+    path = "internal/v1/skus",
+    configuration = OpenFeignConfig.class)
+public interface CartProductFeignClient {
+
+  @GetMapping("/{skuId}")
+  void validateSkuForCart(@PathVariable UUID skuId);
+
+  @PostMapping
+  List<CartItemProducInfo> getCartItemProducts(List<UUID> skuIds);
+}
