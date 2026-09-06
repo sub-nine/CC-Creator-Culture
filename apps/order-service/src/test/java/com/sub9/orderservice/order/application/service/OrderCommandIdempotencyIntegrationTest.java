ba9ff68122d4d4e367adaeaa7829ddb384a211fd
cpfur18 <cpfur18@gmail.com>
2026-09-06 13:37:36 +0900

    test(order): 장바구니 등록 테스트 코드 작성

--- apps/order-service/src/test/java/com/sub9/orderservice/order/application/service/OrderCommandIdempotencyIntegrationTest.java
+++ apps/order-service/src/test/java/com/sub9/orderservice/order/application/service/OrderCommandIdempotencyIntegrationTest.java
@@ -5,7 +5,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import com.sub9.common.exception.BusinessException;
 import com.sub9.common.identifier.UuidV7Generator;
-import com.sub9.orderservice.order.application.port.output.CartSnapshotPort;
+import com.sub9.orderservice.cart.application.service.CartService;
 import com.sub9.orderservice.order.application.port.output.CouponApplicationPort;
 import com.sub9.orderservice.order.application.port.output.CouponUsagePort;
 import com.sub9.orderservice.order.application.port.output.StockPort;
@@ -57,7 +57,7 @@ import org.testcontainers.postgresql.PostgreSQLContainer;
 })
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 @MockitoBean(types = {
-        CartSnapshotPort.class,
+        CartService.class,
         CouponApplicationPort.class,
         CouponUsagePort.class,
         StockPort.class
