4dbac0032e7f543926d2ab49aff6f2c5644934a9
kth <pni2396@gmail.com>
2026-09-08 12:31:57 +0900

    feat(leaderboard): 리더보드 점수 반영 UseCase 작성

--- apps/product-service/src/main/java/com/sub9/productservice/leaderboard/application/port/in/RecordOrderScoreUseCase.java
+++ apps/product-service/src/main/java/com/sub9/productservice/leaderboard/application/port/in/RecordOrderScoreUseCase.java
@@ -1,7 +1,10 @@
 package com.sub9.productservice.leaderboard.application.port.in;
 
+import com.sub9.productservice.leaderboard.application.model.ProductQuantity;
+
+import java.util.List;
 import java.util.UUID;
 
 public interface RecordOrderScoreUseCase {
-    void recordOrderScore(UUID orderId, UUID productId);
+    void recordOrderScore(UUID orderId, List<ProductQuantity> productQuantities);
 }
--- apps/product-service/src/main/java/com/sub9/productservice/leaderboard/application/port/in/RecordProductViewScoreUseCase.java
+++ apps/product-service/src/main/java/com/sub9/productservice/leaderboard/application/port/in/RecordProductViewScoreUseCase.java
@@ -1,9 +1,10 @@
 package com.sub9.productservice.leaderboard.application.port.in;
 
-import java.time.LocalDate;
-import java.util.Map;
+import com.sub9.productservice.leaderboard.application.model.ProductViewCount;
+
+import java.util.List;
 import java.util.UUID;
 
 public interface RecordProductViewScoreUseCase {
-    void recordProductViewScore(UUID eventId, LocalDate localDate, Map<UUID, Long> productViewCounts);
+    void recordProductViewScore(UUID eventId, List<ProductViewCount> productViewCounts);
 }
