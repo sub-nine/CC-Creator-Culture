09e2824411952fc77aefccf2d7416993dc09b957
cpfur18 <cpfur18@gmail.com>
2026-09-15 15:15:58 +0900

    feat(product): 상품 검색 시 category 포트 변경

---
+++ apps/product-service/src/main/java/com/sub9/productservice/product/application/port/out/product/ProductMetadataPort.java
@@ -0,0 +1,21 @@
+package com.sub9.productservice.product.application.port.out.product;
+
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+public interface ProductMetadataPort {
+
+  // Product에서 페이징 처리 및 조립 예정이므로 누락 방지를 위해 일치하는 Product Id 전체 반환
+  Map<UUID, ProductMetadataInfo> getProductMetadata(List<UUID> productIds);
+
+  record ProductMetadataInfo(
+      List<CategoryInfo> categories,
+      List<HashtagInfo> hashtags
+  ) {
+  }
+
+  record CategoryInfo(UUID productId, UUID categoryId, String name) {}
+
+  record HashtagInfo(UUID productId, UUID hashTagId, String name) {}
+}
